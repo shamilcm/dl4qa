c95ab1a4cd5bbb66dd18a4d1da1ef6acab0ec7a9
Shamil Chollampatt <shamil.cm@gmail.com>
2016-10-18 18:12:56 +0800

    also outputs rank file

--- anssel/evaluator.py
+++ anssel/evaluator.py
@@ -3,9 +3,6 @@ from collections import defaultdict
 
 
 def calc_accuracy(probs, labels):
-    """
-    Written by Tapas. 
-    """
     correct_1 = 0
     total_1=0
     correct_0=0
@@ -21,9 +18,7 @@ def calc_accuracy(probs, labels):
         if probs[i] < cut_off and labels[i] == 0:
             correct_0 += 1
     return float(correct_1) / total_1, float(correct_0) / total_0
-    #print "Avg. Accuracy: ", (float(correct_1) / total_1 + float(correct_0) / total_0)/2
 
-   # print "Finished......"
 
 def print_accuracy(probs, labels):
     accuracy1, accuracy0 = calc_accuracy(probs, labels)
@@ -31,16 +26,22 @@ def print_accuracy(probs, labels):
     print "Accuracy 0: ", accuracy0
     print "Avg. Accuracy: ", (accuracy0 + accuracy1)/2
 
-def get_preds(ref_fname, probs):
+def get_preds(ref_file, probs, out_file=None):
     preds = defaultdict(list)
-    with open(ref_fname) as f:
+    if out_file:
+        f_out = open(out_file,'w')
+    with open(ref_file) as f:
         for line,prob in zip(f,probs):
             line = line.strip()
             ids = line.split()
             qid = int(ids[0])
             aid = int(ids[2])
             label = int(ids[3])
-            preds[ids[0]].append((aid, label,prob))
+            preds[ids[0]].append((aid, label, prob[0]))
+            if out_file:
+                f_out.write(str(qid) + " 0 " + str(aid) + " 0 " + str(prob[0]) + " 0" + "\n")
+    if out_file:
+        f_out.close()
     return preds
 
 def calc_mean_avg_prec(preds):
@@ -88,3 +89,24 @@ def calc_mean_reciprocal_rank(preds):
         mean_reciprocal_rank += reciprocal_rank
     mean_reciprocal_rank /= relQ
     return mean_reciprocal_rank
+
+
+def calc_trigger_fscore(preds, thre=0.1):
+    """
+    precision, recall, fmeasure for the task of answering triggering
+    """
+    gt_cnt, pred_cnt, match_cnt = 0.0, 0.0, 0.0
+    for pred in preds.values():
+        sorted_pred = sorted(pred, key=lambda res: res[2], reverse=True)
+        if sorted_pred[0][2] > thre: 
+            pred_cnt += 1.0
+            if sorted_pred[0][1] == 1:
+                match_cnt += 1.0
+        sorted_gt = sorted(pred, key=lambda res: res[1], reverse=True)
+        if sorted_gt[0][1] == 1:
+            gt_cnt += 1.0
+    prec, reca = match_cnt / pred_cnt, match_cnt / gt_cnt
+    if prec+reca == 0:
+        return prec, reca, 0
+    else:
+        return prec, reca, 2*prec*reca / (prec+reca)
