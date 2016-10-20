b411bcb4390c82fa56bcac8133a7f7b6d725f9ad
Shamil Chollampatt <shamil.cm@gmail.com>
2016-10-20 10:31:38 +0800

    oct 18 update from tapas

--- CNNQAClassifier.py
+++ CNNQAClassifier.py
@@ -1,10 +1,17 @@
 import numpy as np
 from collections import namedtuple
 from keras.models import Sequential, Model
-from keras.layers import Dense,Activation,Merge,Input, Lambda,merge
+from keras.layers import Dense, Activation, Merge, Input, merge,Flatten,Lambda
+from keras.layers.convolutional import Convolution1D
 from keras.optimizers import SGD
-import keras.backend as K
+from keras.layers.pooling import AveragePooling1D
+from Pooling import SumPooling1D
+from keras import backend as K
 import sys
+from anssel import evaluator as Score
+from collections import defaultdict
+
+vec_dim=300
 
 def get_W(word_vecs, k=300):
     """
@@ -43,7 +50,29 @@ def load_bin_vec(fname, vocab):
                 word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
             else:
                 f.read(binary_len)
-    word_vecs["<unk>"] = np.random.uniform(-0.25, 0.25, 300)
+    word_vecs["<unk>"] = np.random.uniform(-0.25, 0.25, vec_dim)
+    return word_vecs
+
+def load_word2vec(fname):
+    """
+    Loads 300x1 word vecs from Google (Mikolov) word2vec
+    """
+    word_vecs = {}
+    with open(fname, "rb") as f:
+        header = f.readline()
+        vocab_size, layer1_size = map(int, header.split())
+        binary_len = np.dtype('float32').itemsize * layer1_size
+        for line in xrange(vocab_size):
+            word = []
+            while True:
+                ch = f.read(1)
+                if ch == ' ':
+                    word = ''.join(word)
+                    break
+                if ch != '\n':
+                    word.append(ch)
+            word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')
+    word_vecs["<unk>"] = np.random.uniform(-0.25, 0.25, vec_dim)
     return word_vecs
 
 def load_vocab(vocab_file):
@@ -94,13 +123,13 @@ def load_stop_words(stop_file):
         stop_words.append(line)
     return stop_words
 
-def load_neural_net_data(samples):
+def load_bag_of_words_based_neural_net_data(samples):
     qsdata = []
     ansdata = []
     labels = []
     for sample in samples:
-        qsvec = get_bag_of_words_vec(sample.QsWords)
-        ansvec=get_bag_of_words_vec(sample.AnsWords)
+        qsvec = get_bag_of_words_based_sentence_vec(sample.QsWords)
+        ansvec=get_bag_of_words_based_sentence_vec(sample.AnsWords)
         qsdata.append(qsvec)
         ansdata.append(ansvec)
         labels.append(sample.Label)
@@ -110,8 +139,8 @@ def load_neural_net_data(samples):
     label_nn = np.array(labels)
     return qsdata_nn,ansdata_nn,label_nn
 
-def get_bag_of_words_vec(words):
-    vec = np.zeros(300, dtype='float32')
+def get_bag_of_words_based_sentence_vec(words):
+    vec = np.zeros(vec_dim, dtype='float32')
     word_count = 0
     for word in words:
         if stop_words.count(word) > 0:
@@ -125,59 +154,159 @@ def get_bag_of_words_vec(words):
     # vec /= word_count
     return vec
 
+def run_neural_model(train_qsdata,train_ansdata,train_label,test_qsdata,test_ansdata,ref_lines):
+    batch_size = 100
+    epoch = 20
+
+    qs_input = Input(shape=(vec_dim,), dtype='float32', name='qs_input')
+    ans_input = Input(shape=(vec_dim,), dtype='float32', name='ans_input')
+    qtm = Dense(output_dim=vec_dim, input_dim=vec_dim, activation='linear')(qs_input)
+    merged = merge([qtm, ans_input], mode='dot', dot_axes=(1, 1))
+    labels = Activation('sigmoid', name='labels')(merged)
+    model = Model(input=[qs_input, ans_input], output=[labels])
+
+    model.compile(loss={'labels': 'binary_crossentropy'}, optimizer=SGD(lr=0.001, momentum=0.9, nesterov=True),
+                  metrics=['accuracy'])
+
+    for c in range(0,epoch):
+        model.fit({'qs_input': train_qsdata, 'ans_input': train_ansdata}, {'labels': train_label}, nb_epoch=1,
+                  batch_size=batch_size)
+        probs = model.predict([test_qsdata, test_ansdata], batch_size=batch_size)
+        line_count = 0
+        pred_lines = defaultdict(list)
+        for ref_line in ref_lines:
+            ref_line = ref_line.replace('\n', '')
+            parts = ref_line.strip().split()
+            qid, aid, lbl = int(parts[0]), int(parts[2]), int(parts[3])
+            pred_lines[qid].append((aid, lbl, probs[line_count]))
+            line_count += 1
+        print "Mean Avg. Precision: ", Score.calc_mean_avg_prec(pred_lines)
+        print "Mean Reciprocal Rank: ", Score.calc_mean_reciprocal_rank(pred_lines)
+
+def run_bag_of_words_model(train_samples,test_samples,ref_lines):
+
+    train_qsdata, train_ansdata, train_label = load_bag_of_words_based_neural_net_data(train_samples)
+    test_qsdata, test_ansdata, test_label = load_bag_of_words_based_neural_net_data(test_samples)
+    run_neural_model(train_qsdata,train_ansdata,train_label,test_qsdata,test_ansdata,ref_lines)
+
+
+def get_bigram_data(samples,max_qs_l,max_ans_l):
+    qsdata = np.zeros(shape=(len(samples), max_qs_l, vec_dim), dtype="float32")
+    ansdata = np.zeros(shape=(len(samples), max_ans_l, vec_dim), dtype="float32")
+    labeldata = np.zeros(len(samples), dtype="int32")
+    sent_count = 0
+    for sample in samples:
+        word_count = 0
+        for word in sample.QsWords:
+            if (word_vecs.has_key(word)):
+                qsdata[sent_count][word_count] = word_vecs[word]
+            else:
+                qsdata[sent_count][word_count] = word_vecs["<unk>"]
+            word_count += 1
+        word_count = 0
+        for word in sample.AnsWords:
+            if (word_vecs.has_key(word)):
+                ansdata[sent_count][word_count] = word_vecs[word]
+            else:
+                ansdata[sent_count][word_count] = word_vecs["<unk>"]
+            word_count += 1
+        labeldata[sent_count] = sample.Label
+        sent_count += 1
+    return qsdata,ansdata,labeldata
+
+def run_bigram_model(train_samples, test_samples, ref_lines):
+    max_qs_l = len(train_samples[0].QsWords)
+    for i in range(1, len(train_samples)):
+        if len(train_samples[i].QsWords) > max_qs_l:
+            max_qs_l = len(train_samples[i].QsWords)
+
+    for i in range(0, len(test_samples)):
+        if len(test_samples[i].QsWords) > max_qs_l:
+            max_qs_l = len(test_samples[i].QsWords)
+
+    max_ans_l = len(train_samples[0].AnsWords)
+    for i in range(1, len(train_samples)):
+        if len(train_samples[i].AnsWords) > max_ans_l:
+            max_ans_l = len(train_samples[i].AnsWords)
+
+    for i in range(0, len(test_samples)):
+        if len(test_samples[i].AnsWords) > max_ans_l:
+            max_ans_l = len(test_samples[i].AnsWords)
+
+    Reduce = Lambda(lambda x: x[:, 0, :], output_shape=lambda shape: (shape[0], shape[-1]))
+    train_qsdata,train_ansdata,train_labeldata=get_bigram_data(train_samples,max_qs_l,max_ans_l)
+
+    qs_input = Input(shape=(max_qs_l,vec_dim,), dtype='float32', name='qs_input')
+    qsconvmodel=Convolution1D(nb_filter=vec_dim,filter_length=2,activation="tanh", border_mode='valid')(qs_input)
+    qsconvmodel=AveragePooling1D(pool_length=max_qs_l-1)(qsconvmodel)
+    qsconvmodel=Reduce(qsconvmodel)
+
+    ans_input = Input(shape=(max_ans_l, vec_dim,), dtype='float32', name='ans_input')
+    ansconvmodel=Convolution1D(nb_filter=vec_dim, filter_length=2, activation="tanh", border_mode='valid')(ans_input)
+    ansconvmodel=AveragePooling1D(pool_length=max_ans_l-1)(ansconvmodel)
+    ansconvmodel=Reduce(ansconvmodel)
+
+    batch_size = 100
+    epoch = 5
+
+
+    qtm = Dense(output_dim=vec_dim, input_dim=vec_dim, activation='linear')(qsconvmodel)
+    merged = merge([qtm, ansconvmodel], mode='dot', dot_axes=(1, 1))
+    labels = Activation('sigmoid', name='labels')(merged)
+    model = Model(input=[qs_input, ans_input], output=[labels])
+
+    model.compile(loss={'labels': 'binary_crossentropy'}, optimizer=SGD(lr=0.001, momentum=0.9, nesterov=True),
+                  metrics=['accuracy'])
+
+    test_qsdata, test_ansdata, test_labeldata= get_bigram_data(test_samples,max_qs_l,max_ans_l)
+
+
+    model.fit({'qs_input': train_qsdata, 'ans_input': train_ansdata}, {'labels': train_labeldata}, nb_epoch=epoch, batch_size = batch_size)
+    probs = model.predict([test_qsdata, test_ansdata], batch_size=batch_size)
+    line_count=0
+    pred_lines=defaultdict(list)
+    for ref_line in ref_lines:
+        ref_line=ref_line.replace('\n','')
+        parts = ref_line.strip().split()
+        qid, aid, lbl = int(parts[0]), int(parts[2]), int(parts[3])
+        pred_lines[qid].append((aid, lbl,probs[line_count]))
+        line_count +=1
+    print "Mean Avg. Precision: ", Score.calc_mean_avg_prec(pred_lines)
+    print "Mean Reciprocal Rank: ",Score.calc_mean_reciprocal_rank(pred_lines)
+
 if __name__=="__main__":
 
-    train_file = sys.argv[1]
-    word_vec_file=sys.argv[2]
-    stop_words_file=sys.argv[3]
-    test_file=sys.argv[4]
+    #train_file = sys.argv[1]
+    #word_vec_file=sys.argv[2]
+    #stop_words_file=sys.argv[3]
+    #test_file=sys.argsv[4]
+
+    train_file="data/WikiQASent-train.txt"
+    word_vec_file="data/GoogleNews-vectors-negative300.bin"
+    stop_words_file="data/short-stopwords.txt"
+    test_file="data/WikiQASent-test-filtered.txt"
+    ref_file="data/WikiQASent-test-filtered.ref"
 
     QASample=namedtuple("QASample","QsWords AnsWords Label")
 
-    vocab=load_vocab(train_file)
-    word_vecs=load_bin_vec(word_vec_file,vocab)
-    W,word_idx_map=get_W(word_vecs=word_vecs)
+    #vocab=load_vocab(train_file)
+    #word_vecs=load_bin_vec(word_vec_file,vocab)
+    word_vecs=load_word2vec(word_vec_file)
+    #W,word_idx_map=get_W(word_vecs=word_vecs)
     stop_words=load_stop_words(stop_file=stop_words_file)
 
     train_samples = load_samples(train_file)
-    train_qsdata,train_ansdata,train_label=load_neural_net_data(train_samples)
+    test_samples = load_samples(test_file)
 
-    test_samples=load_samples(test_file)
-    test_qsdata,test_ansdata,test_label=load_neural_net_data(test_samples)
+    file_reader=open(ref_file)
+    ref_lines=file_reader.readlines()
+    file_reader.close()
 
-    batch_size=100
-    epoch=20
+    #Bar of words model
+    #run_bag_of_words_model(train_samples,test_samples,ref_lines)
 
-    qs_input=Input(shape=(300,),dtype='float32',name='qs_input')
-    ans_input = Input(shape=(300,),dtype='float32',name='ans_input')
-    qtm = Dense(output_dim=300,input_dim=300,activation='linear')(qs_input)
-    merged = merge([qtm, ans_input], mode='dot', dot_axes=(1, 1))
-    labels=Activation('sigmoid',name='labels')(merged)
-    model=Model(input=[qs_input,ans_input],output=[labels])
-
-    model.compile(loss={'labels':'binary_crossentropy'}, optimizer=SGD(lr=0.001, momentum=0.9, nesterov=True), metrics=['accuracy'])
-
-    model.fit({'qs_input':train_qsdata,'ans_input':train_ansdata},{'labels':train_label},nb_epoch=epoch,batch_size=batch_size)
-    probs = model.predict([test_qsdata, test_ansdata], batch_size=batch_size)
-
-    correct_1 = 0
-    total_1=0
-    correct_0=0
-    total_0=0
-    cut_off=0.5
-    for i in range(0, len(probs)):
-        if test_label[i]==1:
-            total_1 +=1
-        if probs[i] >= cut_off and test_label[i] == 1:
-            correct_1 += 1
-        if test_label[i]==0:
-            total_0 +=1
-        if probs[i] < cut_off and test_label[i] == 0:
-            correct_0 += 1
-
-    print "Accuracy 1: ", float(correct_1) / total_1
-    print "Accuracy 0: ", float(correct_0) / total_0
-    print "Avg. Accuracy: ", (float(correct_1) / total_1 + float(correct_0) / total_0)/2
+    #Bigram model
+    run_bigram_model(train_samples, test_samples, ref_lines)
 
     print "Finished......"
 
