cc3eb650332db804e2290f748824bcbd77e5c7c6
Shamil Chollampatt <shamil.cm@gmail.com>
2016-10-24 11:59:05 +0800

    added changed data.py

--- anssel/data.py
+++ anssel/data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import logging
+import sys
 
 class Dataset:
 
@@ -34,6 +35,9 @@ class Dataset:
         return vocab
 
     def process(self, embeddings, stop_words):
+        """
+        Returns the training samples and labels as numpy array
+        """
         s1samples_list = []
         s2samples_list = []
         labels_list = []
@@ -44,23 +48,30 @@ class Dataset:
                 s1 = parts[0]           # Question part
                 s2 = parts[1]           # Answer part
                 label = int(parts[2])   # Label
+                # each sample is a list of word vectors
+
                 s1samples_list.append(self.get_sample(s1.split(), embeddings, stop_words))
                 s2samples_list.append(self.get_sample(s2.split(), embeddings, stop_words))
                 labels_list.append(label)
 
-        self.samples = [np.array(s1samples_list),np.array(s2samples_list)]
-        self.labels = np.array(labels_list)
+        self.samples = [s1samples_list,s2samples_list]
+        self.labels = labels_list
+   
 
     def get_sample(self, words, embeddings, stop_words):
         """
         Given a sentence, gets the input in the required format.
         """
+        vecs = []
         vec = np.zeros(embeddings.emb_dim, dtype='float32')
         for word in words:
             if word in stop_words:
                 continue
-            vec += embeddings.get_word_vec(word)
-        return vec
+            vec = embeddings.get_word_vec(word)
+            vecs.append(np.array(vec))
+        if vecs == []:  # if all words are removed as stop words, send a 0 
+            vecs.append(np.zeros(embeddings.emb_dim))
+        return np.array(vecs)
 
 class Embeddings:
     def __init__(self, w2v_fname, vocab):
